bc1f5cda9501e4d7ad617fc668bab2714d21ba37
ivelisousa <44413561+ivelisousa@users.noreply.github.com>
2019-01-28 14:29:44 +0100

    Exercise Solution

---
+++ JS Stalker Exercise/main.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name?");
+var age = prompt("What's your age?");
+
+alert("Hello, " + firstName + " " + lastName);
+alert("Funny! I am " + age + " years old too.");
